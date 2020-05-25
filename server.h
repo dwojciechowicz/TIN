a5f33d9943eaa4efbed4df6b2a0cf74ec3bd1be2
tiago <magda0zych@gmail.com>
2020-05-25 15:54:59 +0200

    Sprzątanie

--- server.h
+++ server.h
@@ -15,6 +15,11 @@
 #define DATE_LENGTH 8 //liczba bajtów na których zapisywana jest data i godzina
 #define BUFFER_SIZE 13 //informacja o dacie, godzinie, typie czujnika, numerze urządzenia i z mierzonej wartości
 
+const int prepare_server(socklen_t *len_addr, int server_port, char* server_ip, bool udplite);
+void process_packet(char *buffer, FILE *file, struct sockaddr_in client);
+void get_packet(const int *socket_addr, FILE *file, socklen_t *len_addr);
+void send_diag(const int socket_, struct sockaddr_in *client, socklen_t len);
+void get_diag_packet(const int *socket_addr, socklen_t *len_addr);
 void* diag_server_func(void* arg);
 
 union bytesInterpretation  //unia potrzebna do wykonywania operacji logicznych na bitach zmiennej typu float
